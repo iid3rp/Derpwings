e54a623b85fc3c0f26df8e965044b624eac55ca3
iid3rp <iid3rp.francismadanlo@gmail.com>
2023-06-01 17:03:26 +0800

    derp's last commit (6/1/23)

--- derpwings! - v1.0/Form1.Designer.cs
+++ derpwings! - v1.0/Form1.Designer.cs
@@ -112,7 +112,6 @@
             this.pictureBox1.Location = new System.Drawing.Point(177, 38);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1033, 328);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
